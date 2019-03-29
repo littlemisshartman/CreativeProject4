f62b3c584e5ec0638c09f916709b5bce1c791b05
Morgan Hartman <littlemiss.hartman@gmail.com>
2019-03-29 14:42:27 -0600

    finish

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -1,23 +1,50 @@
 var leaderboard = new Vue({
-  el: '#leaderboard',
-  data: {
-    items: [],
-  },
-  methods: {
-    async getGames() {
-      try {
-        let res = await axios.get("/api/games");
-        this.items = res.data;
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
+    el: '#leaderboard',
+    data: {
+        items: [],
     },
-    returnToGame: function () {
-      window.location = 'http://localhost:2000/game.html';
-    }
-  },
-  created() {
-    this.getGames();
-  },
-});
+    methods: {
+        async getGames() {
+			try {
+				let res = await axios.get("/api/games");
+				this.items = res.data;
+				return true;
+			} catch (error) {
+				console.log(error);
+			}
+        },
+        async deleteGame(item) {
+			try {
+				console.log(item._id)
+				let response = await axios.delete("/api/games/" + item._id);
+				//this.getGames();
+				return true;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+    },
+    computed: {
+        shortenedItems: function() {
+            if (this.items.length > 10) {
+                var item;
+                var index = 0;
+                while (this.items.length > 10) {
+                    item = this.items[index];
+                    this.deleteGame(item);
+                    this.items.shift();
+                    index++;
+                }
+                var shortenedItemsArray = this.items;
+                return shortenedItemsArray;
+            } else {
+                var shortenedItemsArray = this.items;
+                return shortenedItemsArray;
+            }
+        }
+    },
+    created() {
+      this.getGames();
+    },
+  
+  });
